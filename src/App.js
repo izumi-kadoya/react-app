1caa8314db03ccdd3f6e0e6e6d9a3b46bdb21692
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-10-16 13:48:00 +0900

    3-30

--- src/App.js
+++ src/App.js
@@ -1,53 +1,44 @@
 import React, { Component } from 'react'
 import './App.css'
 
+let data = {title:'React-Context', 
+  message:'this is sample message.'}
+
+const SampleContext = React.createContext(data)
+
 class App extends Component {
-  data = []
-
-  area = {
-    width:"500px",
-    height:"500px",
-    border:"1px solid blue"
-  }
-
-  constructor(props){
-    super(props)
-    this.state = {
-      list:this.data
-    }
-    this.doAction = this.doAction.bind(this)
-  }
-
-  doAction(e){
-    let x = e.pageX
-    let y = e.pageY
-    this.data.push({x:x, y:y})
-    this.setState({
-      list:this.data
-    })
-  }
-
-  draw(d){
-    let s = {
-      position:"absolute",
-      left:(d.x - 25) + "px",
-      top:(d.y - 25) + "px",
-      width:"70px",
-      height:"50px",
-    }
-    return <img src="sirotan.png" alt="Sirotan" style={s} />;
-  }
-  
   render(){
-    return  <div>
-      <h1 className="bg-primary text-white display-4">React</h1>  
+    return <div>
+      <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="subtitle">draw sirotan.</p>
-        <div style={this.area} onClick={this.doAction}>
-          {this.data.map((value)=>this.draw(value))}
-        </div>
+      <Title />
+      <Message />
       </div>
     </div>
+  }
+}
+
+class Title extends Component {
+  static contextType = SampleContext
+
+  render(){
+    return (
+      <div className="card p-2 my-3">
+        <h2>{this.context.title}</h2>
+      </div>
+    )
+  }
+}
+
+class Message extends Component {
+  static contextType = SampleContext
+
+  render(){
+    return (
+      <div className="alert alert-primary">
+        <p>{this.context.message}</p>
+      </div>
+    )
   }
 }
 
