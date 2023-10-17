975e43e46ec6dfad6ae63b70febbc04e4e62a081
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-10-17 15:04:07 +0900

    3−32

--- src/App.js
+++ src/App.js
@@ -1,45 +1,70 @@
 import React, { Component } from 'react'
 import './App.css'
 
-let data = {title:'React-Context', 
-  message:'this is sample message.'}
+let theme = {
+  light:{
+    styles: {
+      backgroundColor:"#f0f9ff",
+      color:"#00f",
+    },
+    head: "bg-primary text-white display-4 mb-4",
+    alert: "alert alert-primary my-3",
+    text: "text-primary m-3",
+    foot: "py-4",
+  },
+  dark:{
+    styles: {
+      backgroundColor:"#336",
+      color:"#eef",
+    },
+    head: "bg-secondary text-white display-4 mb-4",
+    alert: "alert alert-dark my-3",
+    text: "text-light m-3",
+    foot: "py-4",
+  }
+}
 
-const SampleContext = React.createContext(data)
+const ThemeContext = React.createContext(theme.dark) //☆
 
 class App extends Component {
-  render(){
-    return <div>
-      <h1 className="bg-primary text-white display-4">React</h1>
+  static contextType = ThemeContext
+
+    render(){
+    return <div style={this.context.styles}>
+      <h1 className={this.context.head}>React</h1>
       <div className="container">
-      <Title />
-      <Message />
+        <Title value="Content page" />
+        <Message value="This is Content sample." />
+        <Message value="※これはテーマのサンプルです。" />
+        <div className={this.context.foot}></div>
       </div>
     </div>
   }
 }
 
 class Title extends Component {
-  static contextType = SampleContext
+  static contextType = ThemeContext
 
   render(){
     return (
-      <div className="card p-2 my-3">
-        <h2>{this.context.title}</h2>
+      <div className={this.context.alert}>
+        <h2 style={this.context.style}>{this.props.value}</h2>
       </div>
     )
   }
 }
 
 class Message extends Component {
-  static contextType = SampleContext
+  static contextType = ThemeContext
 
   render(){
     return (
-      <div className="alert alert-primary">
-        <p>{this.context.message}</p>
+      <div style={this.context.style}>
+        <p className={this.context.text}>{this.props.value}</p>
       </div>
     )
   }
 }
 
 export default App
+
