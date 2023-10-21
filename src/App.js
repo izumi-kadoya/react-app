5d284226da9ae428df3b56758c413b69cfc621f1
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-10-21 14:40:11 +0900

    4-5

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
 import React, { useState } from 'react'
 import './App.css'
 
+function AlertMessage(props) {
+  return <div className="alert alert-primary h5 text-primary">
+    {props.message}
+  </div>
+}
+
+function CardMessage(props) {
+  return <div className="card p-3 h5 border-primary text-center">
+    {props.message}
+  </div>
+}
+
 function App() {
-  const [count, setCount] = useState(0)
-  const [flag, setFlag] = useState(false)
-  const clickFunc = () => {
-    setCount(count + 1)
-  }
-  const changeFlag = (e) => {
-    setFlag(e.target.checked)
-  }
+  const [msg] = useState("This is sample messsage!")
   
   return (
     <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        {flag ?
-        <div className="alert alert-primary text-center">
-          <p className="h5 mb-3">click: {count} times!</p>
-          <div><button className="btn btn-primary"
-              onClick={clickFunc}>
-            Click me
-          </button></div>
-        </div>
-        :
-        <div className="card p-3 border-primary text-center">
-          <p className="h5 mb-3 text-left text-primary">
-            click: {count} times!</p>
-          <div><button className="btn btn-primary"
-              onClick={clickFunc}>
-            Click me
-          </button></div>
-        </div>
-        }
-        <div className="form-group h6 text-center pt-3">
-          <input type="checkbox" className="form-check-input"
-              id="check1" onChange={changeFlag} />
-          <label className="form-check-label" htmlFor="check1">
-            Change form style.
-          </label>
-        </div>
+        <AlertMessage message={msg} />
+        <CardMessage message={msg} />
       </div>
     </div>
   )
